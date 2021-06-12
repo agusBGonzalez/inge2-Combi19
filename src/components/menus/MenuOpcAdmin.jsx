033ccb914517766e7b99fb4cfd5fb434d02b4066
agusBGonzalez <agusb.gonzalez95@gmail.com>
2021-06-12 15:31:25 -0300

    acomode los menu

--- src/components/menus/MenuOpcAdmin.jsx
+++ src/components/menus/MenuOpcAdmin.jsx
@@ -7,11 +7,11 @@ import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import './SideBar.css';
 
-import { CalendarWeek, Truck, PersonLinesFill, GeoAltFill, Cart4, Map, HouseFill} from 'react-bootstrap-icons';
+import { CalendarWeek, Truck, PersonLinesFill, GeoAltFill, Cart4, Map, HouseFill } from 'react-bootstrap-icons';
 
 
 function MenuOpcAdmin(props) {
-    
+
 
     const historial = useHistory()
 
@@ -22,7 +22,7 @@ function MenuOpcAdmin(props) {
                 const to = '/' + selected;
                 historial.push(to);
             }}
-            style={{ backgroundColor: "#7CA0AF",top: "99px" }}
+            style={{ backgroundColor: "#7CA0AF", top: "99px" }}
         >
             <SideNav.Toggle />
             <SideNav.Nav defaultSelected={props.optionName} >
@@ -30,23 +30,48 @@ function MenuOpcAdmin(props) {
                     <NavIcon>
                         <HouseFill color="black"></HouseFill>
                     </NavIcon>
-                    <NavText style={{color:"black"}}>
+                    <NavText style={{ color: "black" }}>
                         Inicio
+                    </NavText>
+                </NavItem>
+                <NavItem eventKey="listaProductos">
+                    <NavIcon>
+                        <Cart4 color="black"></Cart4>
+                    </NavIcon>
+                    <NavText style={{ color: "black" }}>
+                        Productos
                     </NavText>
                 </NavItem>
                 <NavItem eventKey="listaSitios">
                     <NavIcon>
                         <GeoAltFill color="black"></GeoAltFill>
                     </NavIcon>
-                    <NavText style={{color:"black"}}>
+                    <NavText style={{ color: "black" }}>
                         Sitios
                     </NavText>
                 </NavItem>
+                <NavItem eventKey="listaChoferes">
+                    <NavIcon>
+                        <PersonLinesFill color="black"></PersonLinesFill>
+                    </NavIcon>
+                    <NavText style={{ color: "black" }}>
+                        Choferes
+                    </NavText>
+                </NavItem>
+                <NavItem eventKey="listaCombis">
+                    <NavIcon>
+                        <Truck color="black"></Truck>
+                    </NavIcon>
+                    <NavText style={{ color: "black" }}>
+                        Combis
+                    </NavText>
+                </NavItem>
+
                 <NavItem eventKey="listaRutas">
                     <NavIcon>
                         <Map color="black"></Map>
                     </NavIcon>
-                    <NavText style={{color:"black"}}>
+                    <NavText style={{ color: "black" }}>
                         Rutas
                     </NavText>
                 </NavItem>
@@ -54,38 +79,14 @@ function MenuOpcAdmin(props) {
                     <NavIcon>
                         <CalendarWeek color="black"></CalendarWeek>
                     </NavIcon>
-                    <NavText style={{color:"black"}}>
+                    <NavText style={{ color: "black" }}>
                         Viajes
-                    </NavText>
-                </NavItem>
-                <NavItem eventKey="listaCombis">
-                    <NavIcon>
-                        <Truck color="black"></Truck>
-                    </NavIcon>
-                    <NavText style={{color:"black"}}>
-                        Combis
-                    </NavText>
-                </NavItem>
-                <NavItem eventKey="listaChoferes">
-                    <NavIcon>
-                        <PersonLinesFill color="black"></PersonLinesFill>
-                    </NavIcon>
-                    <NavText style={{color:"black"}}>
-                        Choferes
-                    </NavText>
-                </NavItem>
-                <NavItem eventKey="listaProductos">
-                    <NavIcon>
-                        <Cart4 color="black"></Cart4>
-                    </NavIcon>
-                    <NavText style={{color:"black"}}>
-                        Productos
                     </NavText>
                 </NavItem>
             </SideNav.Nav>
         </SideNav>
-       
+
     );
-  }
-  
-  export default MenuOpcAdmin;
+}
+
+export default MenuOpcAdmin;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import AppFinal from './AppFinal.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'animate.css/animate.min.css'
+import 'animate.css/animate.css'
 
 ReactDOM.render(
 
