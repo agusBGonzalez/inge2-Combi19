eb0691e34451f09e24011981ad7ce07e91c7b619
ZackPiscopo <zj.piscopo@gmail.com>
2021-06-15 12:15:46 -0300

    actualizacion datos gold

--- src/pages/usuario/UsuarioGoldDatosPage.jsx
+++ src/pages/usuario/UsuarioGoldDatosPage.jsx
@@ -1,21 +1,12 @@
 import React,{useState, useEffect} from 'react'
 import { store,auth } from '../../firebaseconf'
 import {Modal, Button, Alert} from 'react-bootstrap'
-import { Link, useHistory} from 'react-router-dom'
+// import { Link, useHistory} from 'react-router-dom'
 import MenuUsuario from '../../components/menus/MenuUsuario'
 import MenuOpcUsuario from '../../components/menus/MenuOpcUsuario'
 
 
 function UsuarioGoldDatosPage() {
-
-    
-
-    const historial = useHistory()
-	const [email, setEmail] = useState ('')
-	const [nombres, setNombres] = useState ('')
-	const [apellido, setApellido] = useState ('')
-	const [fecha, setFecha] = useState ('')
-	const [password, setPassword] = useState ('')
 
     const [numTarjeta, setNumTarjeta] = useState ('')
     const [codTarjeta, setCodTarjeta] = useState ('')
@@ -37,15 +28,21 @@ function UsuarioGoldDatosPage() {
     };
   
 
-	const [msgError, setMsgError] = useState (null)
-
-	//MODAL
-	const [showModal, setShowModal] = useState(false);
-	const handleClose = () => setShowModal(false);
-
 	//ALERT
 	const [showAlert, setShowAlert] = useState(false);
 	const handleCloseAlert = () => {setShowAlert(false); setMsgError(null);}
+    const [msgError, setMsgError] = useState (null)
+
+    //ALERT SUCESS
+    const [showAlertSucc, setShowAlertSucc] = useState(false)
+    const handleCloseAlertSucc = () => setShowAlertSucc(false)
+    const [msgSucc, setMsgSucc] = useState (null)
+ 
+
+//    //ALERT
+//    const [showAlert, setShowAlert] = useState(false);
+//    const handleCloseAlert = () => {setShowAlert(false); setMsgError(null);}
+//    const [msgError, setMsgError] = useState (null)
 
 
     useEffect( () =>{
@@ -89,6 +86,91 @@ function UsuarioGoldDatosPage() {
         
     },[])
 
+    const getUsuarioConfig =  () => {
+        store.collection('usuariosConfig').get()
+        .then(response => {
+            const fetchedUsers = [];
+            response.docs.forEach(document => {
+            const fetchedUser = {
+                id: document.id,
+                ...document.data()
+            };
+            fetchedUsers.push(fetchedUser)
+            });
+            setUsuarios(fetchedUsers)
+        })
+    }
+
+
+    const confirmarEdicion = async (e) => {
+        e.preventDefault();
+
+        if (!numTarjeta.trim() || (numTarjeta.trim().length < 16)){
+            setMsgError('Debe ingresar los 16 números de su tarjeta para crear un usuario Gold')
+            setShowAlert(true)
+            return
+        }
+
+        if (!codTarjeta.trim() || (codTarjeta.trim().length < 3)){
+            setMsgError('Debe ingresar el código de seguridad de la tarjeta, deben ser al menos 3 caracteres')
+            setShowAlert(true)
+            return
+        }
+
+        if (!fechaTarjeta.trim()){
+            setMsgError('Debe ingresar la fecha de vencimiento de su tarjeta')
+            setShowAlert(true)
+            return
+        }
+
+        //VALIDACION DE FECHA DE LA TARJETA
+        const cortoAnioFecTarjeta = fechaTarjeta.substr(0, fechaTarjeta.indexOf('-'))
+        const cortoMesFecTarjeta = fechaTarjeta.substring(fechaTarjeta.indexOf('-') +1 ,fechaTarjeta.length)                
+
+        const numMesTarj = Number(cortoMesFecTarjeta)
+        const numAnioTarj = Number(cortoAnioFecTarjeta)
+
+        // LO DEJO POR EL PROBLEMA QUE TUVO AGUS EN LA DEMO
+        const diaHoyNumb = 15
+        const mesHoyNumb = 6
+        const anioHoyNumb = 2021
+
+        if((anioHoyNumb > numAnioTarj) || ((mesHoyNumb > numMesTarj) && (anioHoyNumb === numAnioTarj)) ) {
+            setMsgError('La fecha de vencimiento de su tarjeta ya no es válida para poder realizar la operación')
+            setShowAlert(true)
+            return
+
+        }
+
+        const editUser = {
+            nombres: usuario.nombres,
+            apellido: usuario.apellido,
+            fechaNac: usuario.fechaNac,
+            email: usuario.email,
+            tipo: "usuario",
+            esGold: usuario.esGold,
+            tarjetaNum: numTarjeta,
+            tarjetaCod: codTarjeta,
+            tarjetaVen: fechaTarjeta,
+            idUser: usuario.idUser,
+            password:usuario.password
+        }
+
+        try{
+            
+            await store.collection('usuariosConfig').doc(idUser).set(editUser)
+            getUsuarioConfig()
+            setMsgSucc('Actualizacion Exitosa! Click aqui para cerrar')
+            setShowAlertSucc(true)
+
+        }catch(e){
+            setMsgError('Uups! Hubo un problema al actualizar los datos en el sistema')
+            setShowAlert(true)
+            console.log(e)
+        }    
+
+    }
+
 
     return (
         <div>
@@ -98,7 +180,7 @@ function UsuarioGoldDatosPage() {
                 <h3 style={{top: 110, position: 'absolute', left: 80,width: "60%",}}> Datos Gold</h3>
                 <button className="btn btn-dark btn-block" type = "submit">Actualizar</button>
                 { showGoldInfo ?
-                <Button style={{top: 105, position: 'absolute', right:170, width: "200px", height: "40px"}} variant="warning" onClick={(e) => { console.log('A') }} > Actualizar Datos Gold </Button>
+                <Button style={{top: 105, position: 'absolute', right:170, width: "200px", height: "40px"}} variant="warning" onClick={confirmarEdicion} > Actualizar Datos Gold </Button>
                 :
                 <Button variant="warning" style={{top: 105, position: 'absolute', right:170, width: "200px", height: "40px"}} disabled> Adherirme a suscripción</Button>
                 }
@@ -160,6 +242,9 @@ function UsuarioGoldDatosPage() {
                      
                     <Alert className="mt-4" variant="danger" show={showAlert} onClick = {handleCloseAlert}>
                         {msgError}
+                    </Alert>
+                    <Alert id="success" className="" variant="success" show={showAlertSucc} onClick={handleCloseAlertSucc} style={{bottom:0,zIndex:5, position: 'absolute', left: 75,width: "60%"}} >
+                        {msgSucc}
                     </Alert>	
                 </div>
                 
